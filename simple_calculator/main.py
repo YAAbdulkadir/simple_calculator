d47834f7cc25d1d8c15d3fcd8cab5bd1ee7fd649
Yasin Abdulkadir <yabdulkadir@mednet.ucla.edu>
2024-08-23 18:29:50 -0700

    step-5-multiply-two-numbers-smart

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
 
 
 class SimpleCalculator:
@@ -8,5 +9,8 @@ class SimpleCalculator:
     def sub(self, a, b):
         return a - b
 
-    def mul(self, a, b):
-        return a * b
+    def mul(self, *args):
+        def mul2(a, b):
+            return a * b
+
+        return reduce(mul2, args)
